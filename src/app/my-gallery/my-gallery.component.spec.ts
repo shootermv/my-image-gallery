ca315c2a41f35d58f4b007a907fc2cea0aafa77d
moshe vilner <shootermv@gmail.com>
2020-03-07 21:21:11 +0200

    adding 'filter' test

--- src/app/my-gallery/my-gallery.component.spec.ts
+++ src/app/my-gallery/my-gallery.component.spec.ts
@@ -39,28 +39,35 @@ describe('MyGalleryComponent', () => {
 
   it('paginateImages method - should 20 is perPage set to 20', () => {
     let images = new Array(20).fill({}), perPage = 20, currentPage = 1, term = '', sortBy = null;
-    let paginated  = component.paginateImages(images, perPage, currentPage, term = '', sortBy);
+    let paginated  = component.paginateImages(images, perPage, currentPage, term, sortBy);
     expect(paginated.length).toBe(20);
   });
 
   it('paginateImages method - should show 5 images when perPage set to 5 ', () => {
     let images = new Array(20).fill({}), perPage = 5, currentPage = 1, term = '', sortBy = null;
-    expect(component.paginateImages(images, perPage, currentPage, term = '', sortBy).length).toBe(5);
+    expect(component.paginateImages(images, perPage, currentPage, term, sortBy).length).toBe(5);
   }); 
   
   it('paginateImages method - should show 5 images when perPage set to 5 and currPage is  2', () => {
     let images = new Array(20).fill({}), perPage = 5, currentPage = 2, term = '', sortBy = null;
-    let paginated  = component.paginateImages(images, perPage, currentPage, term = '', sortBy);
+    let paginated  = component.paginateImages(images, perPage, currentPage, term, sortBy);
     expect(paginated.length).toBe(5);
   
   });
   
-  it('paginateImages method - if currPage is 2 and perPAge is 5 - the first item should have index 5', () => {
+  it('paginateImages method - if currPage is 2 and perPage is 5 - the first item should have index 5', () => {
     let images = new Array(20).fill({}), perPage = 5, currentPage = 2, term = '', sortBy = null;
     images = images.map((item, idx) => ({id: idx}));
-    let paginated  = component.paginateImages(images, perPage, currentPage, term = '', sortBy);
+    let paginated  = component.paginateImages(images, perPage, currentPage, term, sortBy);
     expect(paginated.length).toBe(5);
     expect(paginated[0].id).toBe(5);
   });
 
+  it('paginateImages method - if perPage is 5 - should show 2 if after filtering is 2', () => {
+    let images = new Array(20).fill({}), perPage = 5, currentPage = 1, term = 'a', sortBy = null;
+    images = images.map((item, idx) => ({id: idx, title: [18, 19].includes(idx) ? 'a' : 'z'}));
+    let paginated  = component.paginateImages(images, perPage, currentPage, term, sortBy);
+
+    expect(paginated.length).toBe(2);
+  });
 });
